9f26284e447bd91de87ebd2c620019c22844dbd8
More33 <aymerickngassa@icloud.com>
2025-06-26 15:02:09 +0100

    Amelioration

---
+++ site-presentation-ngassa/script.js
@@ -0,0 +1,7 @@
+// Menu burger toggle
+const burger = document.getElementById('burger');
+const navLinks = document.getElementById('nav-links');
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
